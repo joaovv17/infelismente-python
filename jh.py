d55ced07dd6c0137bd964ea9a3e45244a2591c21
joaovv17 <joaovibarboa@gmail.com>
2025-10-01 17:39:39 -0300

    Add files via upload

---
+++ jh.py
@@ -0,0 +1,17 @@
+idade = 20
+
+if idade > 18:
+    print("voce e maior de idade")
+    print("bloco g")
+else :
+    print("voce é de menor")
+    print("bloco h")
+
+
+for i in range(0,10):
+    print(i)
+
+contador = 1
+while contador <= 10:
+        print(contador)
+        contador+=1
